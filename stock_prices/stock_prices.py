ffdb4196f563b2b9dcae4f5d78ddf5fe726e4821
Nicole Adams <nicoleadams.boston@gmail.com>
2020-04-15 17:20:45 -0400

    add stock_prices

--- stock_prices/stock_prices.py
+++ stock_prices/stock_prices.py
@@ -3,7 +3,21 @@
 import argparse
 
 def find_max_profit(prices):
-  pass
+   # this is the max_profit_so_far, later described as the difference between j and i
+    max_profit = prices[1] - prices[0] 
+      
+    for i in range( 0, len(prices) ): 
+        for j in range( i+1, len(prices) ): 
+            if(prices[j] - prices[i] > max_profit):  
+                max_profit = prices[j] - prices[i] 
+      
+    return max_profit 
+
+
+
+
+
+
 
 
 if __name__ == '__main__':
